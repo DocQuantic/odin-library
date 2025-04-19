7c3566e50e957c2bc6b62201754337ef8af74469
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-04-19 21:58:26 +0200

    Create library basic logic

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+const myLibrary = [];
+
+function Book(title, author, pages, isRead, id){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.id = id;
+    
+    this.info = function() {
+      this.read ? readState="read" : readState="not read yet";
+      info = `${this.title} by ${this.author}, ${this.pages} pages, ${readState}`;
+      return info;
+    }
+}
+
+function addBookToLibrary(title, author, pages, isRead){
+    const id = crypto.randomUUID();
+    let book = new Book(title, author, pages, isRead, id);
+
+    myLibrary.push(book);
+}
+
+addBookToLibrary("The Witcher", "Me", 234, true);
+console.log(myLibrary);
+console.log(myLibrary[0].info())
